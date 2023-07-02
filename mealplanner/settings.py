d9f45667b9c47939f9c2a377387e04d426f5688a
Larz <larsmith11@gmail.com>
2023-07-01 20:13:32 -0400

    update settings

--- mealplanner/settings.py
+++ mealplanner/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dd581x-ajja3+%t(armpc@wmnvk_yeiz_e2w@+st284f@hw7=l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
 
 
 # Application definition
